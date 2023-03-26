9dce255d4c8e25c64cac7766e8f70d6d2cca8d47
aozgokme <74674469+aozgokmen@users.noreply.github.com>
2023-03-26 21:36:12 +0300

    Add files via upload

---
+++ Books/Books/BookContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Books
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book>? Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+      
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-7V9PI8DR\SQLEXPRESS;Database=book;Trusted_Connection=True;Trust Server Certificate=true;");
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Publisher)
+                .WithMany(p => p.Books)
+                .HasForeignKey(b => b.PublisherId);
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublisherId { get; set; }
+        public virtual Publisher Publisher { get; set; }
+        public Book()
+        {
+            Publisher = new Publisher();
+        }
+    }
+     
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+
+    }
+
+
+}
+
+
---
+++ Books/Books/Program.cs
@@ -0,0 +1,41 @@
+﻿using Books;
+using System;
+using System.Linq;
+
+namespace Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BookContext())
+            {
+                // Insert book
+                var book = new Book { Title = "A Game of Thrones", Publisher = new Publisher { Name = "Ahmetcan" } };
+                context.Books.Add(book);
+                context.SaveChanges();
+
+                // Update book
+                var bookToUpdate = context.Books.FirstOrDefault(b => b.Id == book.Id);
+                if (bookToUpdate != null)
+                {
+                    bookToUpdate.Title = "A Clash of Kings";
+                    context.SaveChanges();
+                }
+
+                // Get book with Id
+                var bookWithId = context.Books.FirstOrDefault(b => b.Id == book.Id);
+                Console.WriteLine($"Book with Id {book.Id}: {book.Title}");
+
+                // Get all books
+                var allBooks = context.Books.ToList();
+                Console.WriteLine("All books:");
+                foreach (var b in allBooks)
+                {
+                    Console.WriteLine($"{b.Title} ({b.Publisher.Name})");
+                }
+            }
+        }
+    }
+}
+
